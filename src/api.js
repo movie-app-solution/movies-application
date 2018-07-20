fb711fb312de347da290d6bad82e70d789669145
Justin Reich <justinreich.dev@gmail.com>
2018-07-20 14:51:08 -0500

    Add CRUD

--- src/api.js
+++ src/api.js
@@ -2,5 +2,44 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
-  }
+  },
+  addMovie: (movie) => {
+      const url = '/api/movies';
+      const options = {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(movie),
+      };
+      return fetch(url, options)
+          .then(console.log('Post Created!'))
+          .catch(/* handle errors */);
+  },
+  updateMovie: (movie) => {
+      const url = `/api/movies/${movie.id}`;
+      const options = {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(movie),
+      };
+      return fetch(url, options)
+          .then(console.log('Post Updated!'))
+          .catch(/* handle errors */);
+  },
+    deleteMovie: (id) => {
+        const url = `/api/movies/${id}`;
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+        return fetch(url, options)
+            .then(console.log('Post Deleted!'))
+            .catch(/* handle errors */);
+    },
 };
+
--- src/index.js
+++ src/index.js
@@ -1,22 +1,98 @@
-/**
- * es6 modules and imports
- */
-import sayHello from './hello';
-sayHello('World');
+const {getMovies, addMovie, updateMovie, deleteMovie} = require('./api.js');
 
-/**
- * require style imports
- */
+"use strict";
 
-console.log('test');
-const {getMovies} = require('./api.js');
+const buildHTML = (movies) => {
+    $('#movies').html('');
+    movies.forEach(({title, rating, id}) => {
+        const movie = `
+            <article>
+                <h3>${title}</h3>
+                <p>Rating: ${rating}</p>
+                <p>Id: ${id}</p>
+                <button data-dbid='${id}' class="edit-btn btn btn-warning">Edit</button>
+                <button data-dbid='${id}' class="delete-btn btn btn-danger">Delete</button>
+            </article>
+        `;
+        $('#movies').append(movie);
+    });
+};
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+const buildMovies = () => {
+    $('#movies').html('<h1>Loading...</h1>');
+    getMovies().then((movies) => {
+        buildHTML(movies);
+        return movies;
+    }).then((movies) => {
+        $('.edit-btn').click((e) => {
+            const id = $(e.target).data('dbid');
+            const movie = movies.filter((curr) => curr.id === id)[0];
+            $('#update-movie-form').remove();
+            const makeOptions = (selected) => {
+                let html = '';
+                for (let i = 1; i < 6; i += 1) {
+                    html += `<option value="${i}" ` + ((selected === i) ?  'selected': '') + `>Rating ${i}</option>`;
+                }
+                return html;
+            };
+            $(e.target).parent().append(
+                `
+                <section id="movie-update-form">
+                    <form action="" id="update-movie-form">
+                        <label for="title">Movie Title</label>
+                        <input id="update-title" name="title" type="movie-title" value="${movie.title}">
+                        <label for="rating">Movie Rating</label>
+                        <select name="rating" id="update-rating">
+                `
+                +
+                        makeOptions(+movie.rating)
+                +
+                `
+                        </select>
+                        <button id="update-movie-btn" class="btn btn-warning">Update Movie</button>
+                    </form>
+                </section>
+                `
+            );
+            $('#update-movie-btn').click((e) => {
+                e.preventDefault();
+                console.log($('#update-movie-form').serializeArray());
+                const updatedMovie = $('#update-movie-form').serializeArray()
+                    .reduce((prev, curr) => {
+                        const key = curr['name'];
+                        prev[key] = curr['value'];
+                        return prev;
+                    }, {});
+                updatedMovie.id = +movie.id;
+                updateMovie(updatedMovie).then(buildMovies);
+            });
+
+        });
+        $('.delete-btn').click((e) => {
+            const id = $(e.target).data('dbid');
+            const movie = movies.filter((curr) => curr.id === id)[0];
+            deleteMovie(movie.id).then(buildMovies);
+        });
+    }).catch((error) => {
+      alert('Oh no! Something went wrong.\nCheck the console for details.');
+      console.log(error);
+    });
+};
+
+buildMovies();
+
+$('#add-movie-btn').click(function(e) {
+    e.preventDefault();
+    const newMovie = $('#add-movie-form').serializeArray()
+        .reduce((prev, curr) => {
+            const key = curr['name'];
+            prev[key] = curr['value'];
+            return prev;
+        }, {});
+    addMovie(newMovie).then(buildMovies);
 });
+
+
+
+
+
